4097dd43bccbe3a9bc1dadaa29a2c97e762f79f8
tharinduperera <tharindudananjaya199818@gmail.com>
2020-08-06 01:49:02 +0530

    create payment entity

--- src/lk/ijse/student_management/entity/Payment.java
+++ src/lk/ijse/student_management/entity/Payment.java
@@ -1,5 +1,97 @@
 package lk.ijse.student_management.entity;
 
+import java.math.BigDecimal;
+
 public class Payment {
 
+
+    private String pid;
+    private String rid;
+    private String nic;
+    private String bid;
+    private String ptype;
+    private String pdate;
+    private BigDecimal amount;
+
+    public Payment() {
+    }
+
+    public Payment(String pid, String rid, String nic, String bid, String ptype, String pdate, BigDecimal amount) {
+        this.pid = pid;
+        this.rid = rid;
+        this.nic = nic;
+        this.bid = bid;
+        this.ptype = ptype;
+        this.pdate = pdate;
+        this.amount = amount;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
+
+    public String getRid() {
+        return rid;
+    }
+
+    public void setRid(String rid) {
+        this.rid = rid;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public String getBid() {
+        return bid;
+    }
+
+    public void setBid(String bid) {
+        this.bid = bid;
+    }
+
+    public String getPtype() {
+        return ptype;
+    }
+
+    public void setPtype(String ptype) {
+        this.ptype = ptype;
+    }
+
+    public String getPdate() {
+        return pdate;
+    }
+
+    public void setPdate(String pdate) {
+        this.pdate = pdate;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "pid='" + pid + '\'' +
+                ", rid='" + rid + '\'' +
+                ", nic='" + nic + '\'' +
+                ", bid='" + bid + '\'' +
+                ", ptype='" + ptype + '\'' +
+                ", pdate='" + pdate + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
