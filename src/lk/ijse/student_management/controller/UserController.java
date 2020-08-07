b2d250f8049a18dee27c1e4bc879366f4274d208
tharinduperera <tharindudananjaya199818@gmail.com>
2020-08-08 04:22:50 +0530

    design some user interfaces

--- src/lk/ijse/student_management/controller/UserController.java
+++ src/lk/ijse/student_management/controller/UserController.java
@@ -1,4 +1,11 @@
 package lk.ijse.student_management.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class UserController {
+    public AnchorPane root;
+
+    public void txtusernameOnAction(ActionEvent actionEvent) {
+    }
 }
